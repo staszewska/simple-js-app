194912940cbf9a029dd789af43ae6580f7bfb4ff
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-03-06 23:47:09 +0100

    a pokemonList added to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+  { name: "Bulbasaur", height: 8, types: ["grass", "poison"] },
+  { name: "Pikachu", height: 0.4, types: ["electric", "psychic"] },
+  { name: "Seadra", height: 1.2, types: ["dragon", "ice"] },
+  { name: "Lapras", height: 2.5, types: ["ghost", "water"] },
+  { name: "Caterpie", height: 0.3, types: ["electric", "normal"] },
+];
