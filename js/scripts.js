223c59fbb7a182e1ab55f8b32284057ad4b686f1
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-03-08 15:41:25 +0100

    list of pokemons created in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,26 @@ let pokemonList = [
   { name: "Pikachu", height: 0.4, types: ["electric", "psychic"] },
   { name: "Seadra", height: 1.2, types: ["dragon", "ice"] },
   { name: "Lapras", height: 2.5, types: ["ghost", "water"] },
-  { name: "Caterpie", height: 0.3, types: ["electric", "normal"] },
+  { name: "Caterpie", height: 0.8, types: ["electric", "normal"] },
 ];
+
+// "for" loop itarates over the elements of the "pokemonList" array
+for (let i = 0; i < pokemonList.length; i++) {
+  //contant variable with Pokemon's name and height
+  const standardMessage = `${pokemonList[i].name} (height ${pokemonList[i].height})`;
+
+  // if condition is true, display following
+  if (pokemonList[i].height > 5) {
+    document.write(`<div>${standardMessage} - Wow, that's big! </div>`);
+
+    // otherwise, display following
+  } else {
+    document.write(
+      // displays Pokemon's name and height
+      // pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " "
+
+      // displays Pokemon's name and heigth in a div tag using template literal
+      `<div>${standardMessage}</div>`
+    );
+  }
+}
